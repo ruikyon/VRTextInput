19fbb89020445a87d8832483e2966615d646dc92
ruikyon <meranazunx@gmail.com>
2021-07-19 12:11:26 +0900

    proto implement (before 3 test)

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dx = Input.GetAxisRaw("Horizontal");
+        var dy = Input.GetAxisRaw("Vertical");
+        transform.localPosition = new Vector3(dx * 100 - 200, dy * 100 - 100, 0);
+    }
+}
---
+++ Assets/Scripts/KeyBoardOutput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBoardOutput : MonoBehaviour
+{
+    [SerializeField] Keyboard keyboard;
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = keyboard.Value;
+    }
+}
